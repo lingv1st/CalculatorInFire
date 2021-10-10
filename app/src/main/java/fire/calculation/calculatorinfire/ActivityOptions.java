cdda9f7dedae6309fae05ffcdc68b9716dc8d742
lingv1st <lingv1st.my@yandex.ru>
2021-10-10 13:06:28 +0700

    Добавлено activity настроек с двумя кнопками выбора

---
+++ app/src/main/java/fire/calculation/calculatorinfire/ActivityOptions.java
@@ -0,0 +1,14 @@
+package fire.calculation.calculatorinfire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+}
