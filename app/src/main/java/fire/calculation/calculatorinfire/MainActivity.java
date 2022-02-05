6b21e633c7e562b8868392c4bdbed14c6fd67114
lingv1st <lingv1st.my@yandex.ru>
2022-02-05 20:06:20 +0700

    Исправлены примагничивания (Bias) во всех цепях кнопок (Chain).

--- app/src/main/java/fire/calculation/calculatorinfire/MainActivity.java
+++ app/src/main/java/fire/calculation/calculatorinfire/MainActivity.java
@@ -4,12 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements Constants {
